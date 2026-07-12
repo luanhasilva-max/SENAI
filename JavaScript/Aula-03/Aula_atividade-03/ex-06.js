769d619bb34df50a55d595e9a6ec619ba610c3e1
Luan <luan_ha_silva@estudante.sesisenai.org.br>
2026-07-12 15:43:27 -0300

    atividade

---
+++ JavaScript/Aula-03/Aula_atividade-03/ex-06.js
@@ -0,0 +1,10 @@
+let animais = ['gato', 'cachorro'];
+
+animais.push('pássaro');
+animais.push('peixe');
+console.log(animais); 
+
+let removido = animais.pop();
+console.log(removido); 
+console.log(animais); 
+
---
+++ JavaScript/Aula-03/Aula_atividade-03/ex-07.js
@@ -0,0 +1,9 @@
+let animais = ['gato', 'cachorro', 'pássaro'];
+
+animais.unshift('leão');
+console.log(animais); 
+
+
+let removido = animais.shift();
+console.log('Elemento removido:', removido); 
+console.log(animais);
